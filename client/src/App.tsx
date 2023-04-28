01127033bf71c5f2f09048ab9f2a76920544d2f5
unknown <nurkenkenes7@gmail.com>
2023-04-28 22:13:49 +0600

    empty commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,21 +8,21 @@ import Navbar from './components/Navbar/Navbar';
 function App() {
 
     return (
-        <div>
-            <div className='w-full z-30 fixed'>
-                <Navbar />
-            </div>
-            <div className='flex' id="outer-container">
-                <ProSidebarProvider>
-                    <CustomSidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'}/>
-                </ProSidebarProvider>
-                <div id="detail" className=' mt-8 w-[100vw]'>
-                    <div id='page-wrap' className=' mt-20 w-[100vw] items-center flex flex-col'>
-                        <Outlet />
-                    </div>
-                </div>
-            </div>
-        </div>
+      <div>
+          <div className='w-full z-30 fixed'>
+              <Navbar />
+          </div>
+          <div className='flex' id="outer-container">
+              <ProSidebarProvider>
+                  <CustomSidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'}/>
+              </ProSidebarProvider>
+              <div id="detail" className=' mt-8 w-[100vw]'>
+                  <div id='page-wrap' className=' mt-20 w-[100vw] items-center flex flex-col'>
+                      <Outlet />
+                  </div>
+              </div>
+          </div>
+      </div>
     );
 }
 
