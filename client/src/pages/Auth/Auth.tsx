3ddaa2a68c3cc24b335bc95e1ff1290a9b455145
unknown <nurkenkenes7@gmail.com>
2023-04-30 23:52:55 +0600

    added auth page with clerk auth forms

---
+++ client/src/pages/Auth/Auth.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Auth = ({ children } : any) => {
+  return (
+    <div className='flex justify-center py-8'>
+      {children}
+    </div>
+  )
+}
+
+export default Auth
