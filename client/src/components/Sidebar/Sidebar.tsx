515b791a9df0af91f793ea25826046441523b14e
NurkenInc <nurkenkenes7@gmail.com>
2023-05-03 22:45:03 +0600

    added drawer content

--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React, { RefAttributes, useRef } from 'react'
+import React, { useRef } from 'react'
 import {
   Drawer,
   DrawerBody,
@@ -8,32 +8,44 @@ import {
   DrawerContent,
   DrawerCloseButton,
   Button,
+  Input,
   useDisclosure
 } from '@chakra-ui/react'
+import { AiOutlineMenu } from 'react-icons/ai'
 
 const Sidebar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure()
   const openBtnRef = useRef<any>() 
 
   return (
-    <aside>
-      <Button onClick={onOpen} ref={openBtnRef}>BurgerMenu icon</Button>
-      <Drawer
-        isOpen={isOpen}
-        placement='right'
-        onClose={onClose}
-        finalFocusRef={openBtnRef}
+    <>
+      <Button ref={openBtnRef} colorScheme='teal' onClick={onOpen}>
+        Open
+      </Button>
+        <Drawer
+          isOpen={isOpen}
+          placement='right'
+          onClose={onClose}
+          finalFocusRef={openBtnRef}
         >
           <DrawerOverlay />
           <DrawerContent>
             <DrawerCloseButton />
-            <DrawerHeader>Listener</DrawerHeader>
-            <DrawerBody>
+            <DrawerHeader>Create your account</DrawerHeader>
 
+            <DrawerBody>
+              <Input placeholder='Type here...' />
             </DrawerBody>
+
+            <DrawerFooter>
+              <Button variant='outline' mr={3} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button colorScheme='blue'>Save</Button>
+            </DrawerFooter>
           </DrawerContent>
-      </Drawer> 
-    </aside>
+        </Drawer>
+    </>
   )
 }
 
