26777069f1acb655072920bec82727991da42044
NurkenInc <nurkenkenes7@gmail.com>
2023-05-05 23:18:09 +0600

    added folders and notes

--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -9,14 +9,46 @@ import {
 } from '@chakra-ui/react'
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
 
+const testNotes = [
+  {
+    name: 'etc',
+    subnotes: [
+      {
+        name: 'idk'
+      },
+      {
+        name: 'whatever'
+      },
+      {
+        name: 'lol'
+      },
+    ]
+  },
+  {
+    name: 'whatever'
+  },
+  {
+    name: 'idontcare'
+  },
+  {
+    name: 'tatatata'
+  }
+]
+
 const Sidebar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure()
   const openBtnRef = useRef<any>() 
 
   return (
     <>
-      <Button ref={openBtnRef} colorScheme='teal' onClick={onOpen}>
-        <AiOutlineMenu />
+      <Button
+        ref={openBtnRef}
+        colorScheme='teal'
+        onClick={onOpen}
+        position={'fixed'}
+        top={11}
+      >
+        <AiOutlineMenu size={30} />
       </Button>
         <Drawer
           isOpen={isOpen}
@@ -28,22 +60,47 @@ const Sidebar = () => {
           <DrawerContent>
             
             <DrawerBody>
-              <div className='bg-black w-[30%] h-[100vh]'>
+              <div className='bg-black w-[200px] h-[100vh]'>
                 <div className='flex justify-between items-center'>
                   <div className='px-8 font-bold text-white text-[20px]'>
                     <h3>Listener</h3>
                   </div>
-                  <div className='text-right p-4'>
+                  <div className='text-right py-4'>
                     <button className='bg-transparent text-white px-2 py-2' onClick={onClose}>
                       <AiOutlineClose />
                     </button>
                   </div>
                 </div>
-                <div className='hover:bg-white py-3 text-white'>General</div>
-                {/* link to general page  */}
-                <div className='hover:bg-white py-3 text-white'>Notes</div>
-                {/* // notes list */}
-                <div className='hover:bg-white py-3 text-white'>Contact us</div>
+                <div>
+                  <div className='hover:bg-white py-3 px-1 text-white'>
+                    General
+                  </div>
+                  {/* link to general page  */}
+                  <div className='hover:bg-white py-3 px-1 text-white'>
+                    Notes
+                  </div>
+                    <div className='text-white'>
+                      {
+                        testNotes.map((item) => (
+                          <div>
+                            <div className='hover:bg-white px-1 py-3'>
+                              {item.name}
+                            </div>
+                            {
+                              item?.subnotes?.map((item) => (
+                                <div className='pl-4 py-3 hover:bg-white'>
+                                  {item.name}
+                                </div>
+                              ))
+                            }
+                          </div>
+                        ))
+                      }
+                    </div>
+                  <div className='hover:bg-white py-3 px-1 text-white'>
+                    Contact us
+                  </div>
+                </div>
               </div>
             </DrawerBody>
            
