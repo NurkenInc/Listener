bedb55e687d7e0c86921b7c58fb2a44d8aa816d8
NurkenInc <nurkenkenes7@gmail.com>
2023-05-06 23:19:01 +0600

    added sidebarfooter component

---
+++ client/src/components/Sidebar/SidebarFooter.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SidebarFooter = () => {
+  return (
+    <div className='hover:bg-white py-3 px-1 text-white'>
+      Contact us
+    </div>
+  )
+}
+
+export default SidebarFooter
