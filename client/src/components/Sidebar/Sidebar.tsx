c3408ecf119499e2f490d686c330d675687cf320
NurkenInc <nurkenkenes7@gmail.com>
2023-05-06 23:19:51 +0600

    separated sidebar to different components

--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -7,33 +7,9 @@ import {
   Button,
   useDisclosure
 } from '@chakra-ui/react'
-import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
+import { AiOutlineMenu } from 'react-icons/ai'
 
-const testNotes = [
-  {
-    name: 'etc',
-    subnotes: [
-      {
-        name: 'idk'
-      },
-      {
-        name: 'whatever'
-      },
-      {
-        name: 'lol'
-      },
-    ]
-  },
-  {
-    name: 'whatever'
-  },
-  {
-    name: 'idontcare'
-  },
-  {
-    name: 'tatatata'
-  }
-]
+import { SidebarFooter, SidebarHeader, SidebarItems } from '../Sidebar'
 
 const Sidebar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure()
@@ -61,46 +37,9 @@ const Sidebar = () => {
             
             <DrawerBody>
               <div className='bg-black w-[200px] h-[100vh]'>
-                <div className='flex justify-between items-center'>
-                  <div className='px-8 font-bold text-white text-[20px]'>
-                    <h3>Listener</h3>
-                  </div>
-                  <div className='text-right py-4'>
-                    <button className='bg-transparent text-white px-2 py-2' onClick={onClose}>
-                      <AiOutlineClose />
-                    </button>
-                  </div>
-                </div>
-                <div>
-                  <div className='hover:bg-white py-3 px-1 text-white'>
-                    General
-                  </div>
-                  {/* link to general page  */}
-                  <div className='hover:bg-white py-3 px-1 text-white'>
-                    Notes
-                  </div>
-                    <div className='text-white'>
-                      {
-                        testNotes.map((item) => (
-                          <div>
-                            <div className='hover:bg-white px-1 py-3'>
-                              {item.name}
-                            </div>
-                            {
-                              item?.subnotes?.map((item) => (
-                                <div className='pl-4 py-3 hover:bg-white'>
-                                  {item.name}
-                                </div>
-                              ))
-                            }
-                          </div>
-                        ))
-                      }
-                    </div>
-                  <div className='hover:bg-white py-3 px-1 text-white'>
-                    Contact us
-                  </div>
-                </div>
+                <SidebarHeader onClose={onClose} />
+                <SidebarItems />
+                <SidebarFooter />
               </div>
             </DrawerBody>
            
