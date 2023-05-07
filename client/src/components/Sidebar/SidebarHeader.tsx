d2e4d28b918bfffeb98d5bcd6de9923522cc04ec
NurkenInc <nurkenkenes7@gmail.com>
2023-05-07 23:30:38 +0600

    changed text ui color added logo to header

--- client/src/components/Sidebar/SidebarHeader.tsx
+++ client/src/components/Sidebar/SidebarHeader.tsx
@@ -4,11 +4,12 @@ import { AiOutlineClose } from 'react-icons/ai'
 const SidebarHeader = ({ onClose } : { onClose: any }) => {
   return (
     <div className='flex justify-between items-center'>
-      <div className='px-8 font-bold text-white text-[20px]'>
-        <h3>Listener</h3>
+      <div className='px-6 flex items-center font-bold text-black text-[20px]'>
+        <img src='/icon.png' className='h-[25px]' />
+        <h3 className='text-blue-400'>Listener</h3>
       </div>
       <div className='text-right py-4'>
-        <button className='bg-transparent text-white px-2 py-2' onClick={onClose}>
+        <button className='bg-transparent text-black px-2 py-2' onClick={onClose}>
           <AiOutlineClose />
         </button>
       </div>
