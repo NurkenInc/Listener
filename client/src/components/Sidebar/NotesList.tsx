ecb6345f27321be5f6be87447e119c1f8dd82883
NurkenInc <nurkenkenes7@gmail.com>
2023-05-09 23:06:40 +0600

    added note name change feature

--- client/src/components/Sidebar/NotesList.tsx
+++ client/src/components/Sidebar/NotesList.tsx
@@ -1,28 +1,41 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, RefAttributes } from 'react'
 import { FiEdit } from 'react-icons/fi'
 import { BsCheck2Square } from 'react-icons/bs'
 import { AiOutlineCloseSquare } from 'react-icons/ai'
 
 const NotesItem = ({ text } : { text: string }) => {
   const [isEditing, setIsEditing] = useState<boolean>(false)
+  const noteInputRef = useRef<any>(null)
+
+  const handleEditing = () =>{
+    setIsEditing(true);
+    console.log(noteInputRef.current);
+    noteInputRef?.current?.focus();
+  }
 
   return (
     <div className='px-1 py-3 hover:bg-white'>
       <div className='flex items-center justify-between'>
-        {text}
+        <input
+          ref={noteInputRef}
+          defaultValue={text} 
+          className='w-[100%] bg-inherit rounded-md'
+          readOnly={!isEditing}
+          // disabled={!isEditing} // todp fix when state changing interrupt focusing
+        />
         {
           isEditing ? (
             <div className='flex'>
-              <div className='bg-slate-300 mr-2 rounded-md px-1 py-1'>
-                <BsCheck2Square size='1rem' onClick={() => setIsEditing(false)} />
+              <div onClick={() => setIsEditing(false)} className='bg-slate-300 mr-2 rounded-md px-1 py-1 hover:bg-slate-400'>
+                <BsCheck2Square size='1rem' />
               </div>
-              <div className='bg-slate-300 mr-2 rounded-md px-1 py-1'>
-                <AiOutlineCloseSquare size='1rem' onClick={() => setIsEditing(false)} />
+              <div onClick={() => setIsEditing(false)} className='bg-slate-300 mr-2 rounded-md px-1 py-1 hover:bg-slate-400'>
+                <AiOutlineCloseSquare size='1rem' />
               </div>
             </div>
           ) : (
-            <div className='bg-slate-300 mr-2 rounded-md px-1 py-1'>
-              <FiEdit size='1rem' onClick={() => setIsEditing(true)} />
+            <div onClick={handleEditing} className='bg-slate-300 mr-2 rounded-md px-1 py-1 hover:bg-slate-400'>
+              <FiEdit size='1rem' />
             </div>
           )
         }
