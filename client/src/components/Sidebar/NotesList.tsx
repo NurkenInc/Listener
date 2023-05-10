bd6777f4196f231e46f8fa974c09e4a70c51ae2a
NurkenInc <nurkenkenes7@gmail.com>
2023-05-10 23:09:04 +0600

    added sidebar show and hide subnotes feature

--- client/src/components/Sidebar/NotesList.tsx
+++ client/src/components/Sidebar/NotesList.tsx
@@ -1,35 +1,65 @@
-import React, { useState, useRef, RefAttributes } from 'react'
+import React, { useState, useRef, useMemo, useCallback } from 'react'
+import { Collapse, useDisclosure } from '@chakra-ui/react'
+
 import { FiEdit } from 'react-icons/fi'
 import { BsCheck2Square } from 'react-icons/bs'
-import { AiOutlineCloseSquare } from 'react-icons/ai'
+import { AiOutlineCloseSquare, AiOutlineArrowRight } from 'react-icons/ai'
 
-const NotesItem = ({ text } : { text: string }) => {
-  const [isEditing, setIsEditing] = useState<boolean>(false)
-  const noteInputRef = useRef<any>(null)
+interface NoteItemProps {
+  text: string,
+  handleToggleSubnotes?: any,
+  isSubnotesOpen: boolean
+}
 
-  const handleEditing = () =>{
+const NotesItem = ({ text, isSubnotesOpen, handleToggleSubnotes } : NoteItemProps) => {
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const noteInputRef = useRef<any>(null);
+
+  const handleEditing = () => {
     setIsEditing(true);
-    console.log(noteInputRef.current);
+    noteInputRef!.current!.disabled = false;
     noteInputRef?.current?.focus();
   }
 
+  const handleSaveChanges = () => {
+    setIsEditing(false);
+    noteInputRef!.current!.disabled = true;
+  }
+
+  const handleCancelChanges = () => {
+    setIsEditing(false);
+    noteInputRef!.current!.value = text;
+    noteInputRef!.current!.disabled = true;
+  }
+
+  // console.log('rerender')
+
   return (
     <div className='px-1 py-3 hover:bg-white'>
-      <div className='flex items-center justify-between'>
-        <input
-          ref={noteInputRef}
-          defaultValue={text} 
-          className='w-[100%] bg-inherit rounded-md'
-          readOnly={!isEditing}
-          // disabled={!isEditing} // todp fix when state changing interrupt focusing
-        />
+      <div className='flex gap-3 items-center justify-between'>
+        <div onClick={handleToggleSubnotes} className='flex'>
+          <input
+            ref={noteInputRef}
+            defaultValue={text}
+            className='w-[100%] bg-inherit rounded-md'
+            readOnly={!isEditing}
+            disabled={true}
+            />
+          {
+            handleToggleSubnotes ? (
+              <div className={`hover:bg-slate-200 p-1 rounded-md ${isSubnotesOpen ? 'rotate-90' : 'rotate-0'} transition-all duration-200`}>
+                <AiOutlineArrowRight size='1rem' />
+              </div>
+            ) : null
+          }
+        </div>
         {
           isEditing ? (
             <div className='flex'>
-              <div onClick={() => setIsEditing(false)} className='bg-slate-300 mr-2 rounded-md px-1 py-1 hover:bg-slate-400'>
+              <div onClick={handleSaveChanges} className='bg-slate-300 mr-2 rounded-md px-1 py-1 hover:bg-slate-400'>
                 <BsCheck2Square size='1rem' />
               </div>
-              <div onClick={() => setIsEditing(false)} className='bg-slate-300 mr-2 rounded-md px-1 py-1 hover:bg-slate-400'>
+              <div onClick={handleCancelChanges} className='bg-slate-300 mr-2 rounded-md px-1 py-1 hover:bg-slate-400'>
                 <AiOutlineCloseSquare size='1rem' />
               </div>
             </div>
@@ -43,10 +73,11 @@ const NotesItem = ({ text } : { text: string }) => {
     </div>
   )
 }
+const MemorizedNoteItem = React.memo(NotesItem)
 
 const NotesList = ({ notes } : { notes: Array<any> }) => {
-  
-  const subnotes = (note : any) => {
+
+  const Subnotes = ({ note }: { note : any }) => {
     return (
       <>
         {
@@ -59,18 +90,31 @@ const NotesList = ({ notes } : { notes: Array<any> }) => {
       </>
     )
   }
+
+  const Note = ({ note } : { note: any }) => {
+    const { isOpen, onToggle } = useDisclosure();
+
+    const handleToggleSubnotes = () => {
+      onToggle();
+    }
+
+
+
+    return (
+      <div>
+        <MemorizedNoteItem text={note.name} isSubnotesOpen={isOpen} handleToggleSubnotes={handleToggleSubnotes} />
+        <Collapse in={isOpen} animateOpacity >
+          <Subnotes note={note} />
+        </Collapse>
+      </div>
+    )
+  }
   
   return (
     <>
       {
         notes.map((item) => (
-          <div key={item.name}>
-            <NotesItem text={item.name} />
-            {/* <div className='hover:bg-white px-1 py-3'>
-              {item.name}
-            </div> */}
-            {subnotes(item)}
-          </div>
+          <Note note={item} key={item.name} />
         ))
       }
     </>
