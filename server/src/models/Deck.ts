3588f4d5ccb394548ac1c0b56105dd60310053b4
NurkenInc <nurkenkenes7@gmail.com>
2023-05-13 23:52:59 +0600

    added createdat field to deck scheme for filters, and id to deck cards for requests

--- server/src/models/Deck.ts
+++ server/src/models/Deck.ts
@@ -1,20 +1,23 @@
+import { ObjectId } from 'mongodb';
 import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 // const ObjectId = mongoose.Types.ObjectId;
 
 const DeckSchema = new Schema({
-    creator: String,
-    title: String,
-    cards: [
-        {
-            name: String,
-            description: String,
-            deadline: Date,
-            status: String,
-            additionalInfo: String,
-        }
-    ],
+  creator: String,
+  createdAt: Date,
+  title: String,
+  cards: [
+      {
+        id: ObjectId,
+        name: String,
+        description: String,
+        deadline: Date,
+        status: String,
+        additionalInfo: String,
+      }
+  ],
 });
 
 const DeckModel = mongoose.model("Deck", DeckSchema);
