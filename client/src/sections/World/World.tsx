c055851f81f04d149be46b9fb0e18902ffd050f9
NurkenInc <nurkenkenes7@gmail.com>
2023-05-13 23:47:42 +0600

    refactored, removed extra string

--- client/src/sections/World/World.tsx
+++ client/src/sections/World/World.tsx
@@ -1,11 +1,9 @@
-'use client';
-
 import { motion } from "framer-motion";
 
-import styles from '../../styles';
-import { staggerContainer, fadeIn } from '../../utils/motion';
+import { TitleText , TypingText } from "@/components/CustomTexts/CustomTexts";
 
-import { TitleText , TypingText } from "../../components/CustomTexts/CustomTexts";
+import { staggerContainer, fadeIn } from '@/utils/motion';
+import styles from '@/styles';
 
 const World = () => (
   <section className={`${styles.paddings} relative z-10`}>
@@ -16,7 +14,6 @@ const World = () => (
       viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex flex-col`}
     >
-
       <TypingText title="| People on the world" textStyles="text-center" />
       <TitleText
         title={(
@@ -25,21 +22,17 @@ const World = () => (
         )}
         textStyles="text-center md:leading-[75px] leading-[35px]"
       />
-
       <motion.div
         variants={fadeIn('up', 'tween', 0.3, 1)}
         className="relative mt-[68px] flex w-full h-[550px]"
       >
         <img src="map.png" alt="map" className="w-full h-full object-cover" />
-
         <div className="absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
           <img src="people-01.png" alt="people" className="w-full h-full" />
         </div>
-
         <div className="absolute top-10 left-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
           <img src="people-02.png" alt="people" className="w-full h-full" />
         </div>
-
         <div className="absolute top-1/2 left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
           <img src="people-03.png" alt="people" className="w-full h-full" />
         </div>
