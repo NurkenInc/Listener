5a536a53c635b966bcfe1a294f77dfc48a7f66ba
NurkenInc <nurkenkenes7@gmail.com>
2023-05-13 23:51:18 +0600

    exported all controllers via index file

---
+++ server/src/controller/decks/index.ts
@@ -0,0 +1,19 @@
+import createCardForDeckController from './createCardForDeckController';
+import createDeckController from './createDeckController';
+import deleteCardOnDeckController from './deleteCardOnDeckController';
+import deleteDeckController from './deleteDeckController';
+import getDeckController from './getDeckController';
+import getDecksController from './getDecksController';
+import updateCardOnDeckController from './updateCardOnDeckController';
+import updateDeckController from './updateDeckController';
+
+export {
+  createCardForDeckController,
+  createDeckController,
+  deleteCardOnDeckController,
+  deleteDeckController,
+  getDeckController,
+  getDecksController,
+  updateCardOnDeckController,
+  updateDeckController
+}
