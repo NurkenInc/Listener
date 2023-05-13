dcc8957570a1c8be8765252d88f19c81c17ed28f
NurkenInc <nurkenkenes7@gmail.com>
2023-05-13 23:26:43 +0600

    added aliases to viteconfig

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { URL, fileURLToPath } from 'url'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
+      { find: '@hook', replacement: fileURLToPath(new URL('./src/hooks', import.meta.url)) },
+    ]
+  },
 })
