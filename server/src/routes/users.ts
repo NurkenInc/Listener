4494a2c0846856f5c6ec1912f2fe15c9f72d457a
NurkenInc <nurkenkenes7@gmail.com>
2023-05-13 23:53:48 +0600

    commented unused code

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,11 +1,11 @@
-import express from 'express';
-import passport from 'passport';
+// import express from 'express';
+// import passport from 'passport';
 
-import { signin, signup } from '../controller/user';
+// import { signin, signup } from '../controller/user';
 
-const router = express.Router();
+// const router = express.Router();
 
-router.post('/signin', signin);
-router.post('/signup', signup);
+// router.post('/signin', signin);
+// router.post('/signup', signup);
 
-export default router;
+// export default router;
