a7bac8fbb19e2bc38ef1b02c08bcf2fcfbc574ed
NurkenInc <nurkenkenes7@gmail.com>
2023-05-13 23:30:48 +0600

    commented unused code

--- client/src/actions/auth.ts
+++ client/src/actions/auth.ts
@@ -1,27 +1,27 @@
-import { AUTH } from '../constants/actionTypes';
-import * as api from '../api/index.js';
-import { API_URL } from '../api/config';
+// import { AUTH } from '../constants/actionTypes';
+// import * as api from '../api/index.js';
+// import { API_URL } from '../api/config';
 
-export const signin = (formData : any, navigate : any) => async (dispatch : any) => {
-  try {1
-    const { data } = await api.signIn(formData);
+// export const signin = (formData : any, navigate : any) => async (dispatch : any) => {
+//   try {
+//     const { data } = await api.signIn(formData);
 
-    dispatch({ type: AUTH, data });
+//     dispatch({ type: AUTH, data });
 
-    navigate('/listener');
-  } catch (error : any) {
-    return error.response.data.message;
-  }
-};
+//     navigate('/listener');
+//   } catch (error : any) {
+//     return error.response.data.message;
+//   }
+// };
 
-export const signup = (formData : any, navigate : any) => async (dispatch : any) => {
-  try {
-    const { data } = await api.signUp(formData);
+// export const signup = (formData : any, navigate : any) => async (dispatch : any) => {
+//   try {
+//     const { data } = await api.signUp(formData);
 
-    dispatch({ type: AUTH, data });
+//     dispatch({ type: AUTH, data });
 
-    navigate('/listener');
-  } catch (error : any) {
-    return error.response.data.message;
-  }
-};
+//     navigate('/listener');
+//   } catch (error : any) {
+//     return error.response.data.message;
+//   }
+// };
