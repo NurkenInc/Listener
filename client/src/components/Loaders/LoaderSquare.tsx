75f410c3bee6da2e023fd2116ab802e061a557b1
NurkenInc <nurkenkenes7@gmail.com>
2023-05-13 23:37:12 +0600

    refactored

--- client/src/components/Loaders/LoaderSquare.tsx
+++ client/src/components/Loaders/LoaderSquare.tsx
@@ -1,11 +1,11 @@
-export default function LoaderSquare() {
-    return (
-        <div className='bg-white'>
-            <img
-                src='loaderSquare.svg'
-                alt='loader'
-                className='h-[175px] w-[175px]'
-            />
-        </div>
-    );
+import React from 'react';
+
+const LoaderSquare = () => {
+  return (
+    <div className='bg-white'>
+      <img src='loaderSquare.svg' alt='loader' className='h-[175px] w-[175px]' />
+    </div>
+  );
 }
+
+export default LoaderSquare;
