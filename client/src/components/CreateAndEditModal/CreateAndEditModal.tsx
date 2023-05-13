d434b9a73b0e9310aecc9f0f39b14d1face4b0ac
NurkenInc <nurkenkenes7@gmail.com>
2023-05-13 23:34:55 +0600

    added props type interface

--- client/src/components/CreateAndEditModal/CreateAndEditModal.tsx
+++ client/src/components/CreateAndEditModal/CreateAndEditModal.tsx
@@ -1,28 +1,29 @@
-import { faSquareCheck } from "@fortawesome/free-solid-svg-icons";
+import React from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSquareCheck } from "@fortawesome/free-solid-svg-icons";
 
-export default function CreateAndEditModal({
-    placeholder,
-    onChange,
-    onClick,
-}: {
-    placeholder: string;
-    onChange: any;
-    onClick: any;
-}) {
-    return (
-        <div className='w-full h-full flex'>
-            <input
-                type='text'
-                name='notename'
-                id='editnotename'
-                placeholder={placeholder}
-                className='bg-gray-50 block w-full p-1 rounded-[10px] text-black'
-                onChange={(event) => onChange(event?.target.value)}
-            />
-            <button className='p-0 w-[40px] bg-white' onClick={onClick}>
-                <FontAwesomeIcon icon={faSquareCheck} />
-            </button>
-        </div>
-    );
+interface CreateAndEditModalProps {
+  placeholder: any,
+  onChange: any,
+  onClick: any
 }
+
+const CreateAndEditModal = ({ placeholder, onChange, onClick}: CreateAndEditModalProps) => {
+  return (
+    <div className='w-full h-full flex'>
+      <input
+        type='text'
+        name='notename'
+        id='editnotename'
+        placeholder={placeholder}
+        className='bg-gray-50 block w-full p-1 rounded-[10px] text-black'
+        onChange={(event) => onChange(event?.target.value)}
+      />
+      <button className='p-0 w-[40px] bg-white' onClick={onClick}>
+        <FontAwesomeIcon icon={faSquareCheck} />
+      </button>
+    </div>
+  );
+}
+
+export default CreateAndEditModal;
