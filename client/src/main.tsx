b79205dcc5191ac519f9b1472fd54fa8cef74181
NurkenInc <nurkenkenes7@gmail.com>
2023-05-13 23:26:21 +0600

    separated store from main file

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import { reducers } from './reducers';
 
+import store from './store';
 import App from './App';
 
 import './index.css';
-
-// separate store
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
