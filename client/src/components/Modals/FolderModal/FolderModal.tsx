a34c640ba92b77799ead0dc9ca0bb6c91cbd3779
NurkenInc <nurkenkenes7@gmail.com>
2023-05-16 22:46:16 +0600

    implemented modal layout for addition of folders

--- client/src/components/Modals/FolderModal/FolderModal.tsx
+++ client/src/components/Modals/FolderModal/FolderModal.tsx
@@ -13,30 +13,29 @@ import {
 
 interface FolderModalProps {
   isOpen: boolean,
+  onClose: any,
 }
 
-const FolderModal = ({ isOpen } : FolderModalProps) => {
-  const { onClose } = useDisclosure();
-  
+const FolderModal = ({ isOpen, onClose } : FolderModalProps) => {
   return (
     <>
       <Modal isOpen={isOpen} onClose={onClose} isCentered>
         <ModalOverlay />
         <ModalContent className='bg-black inline-block text-center align-middle'>
-          <ModalHeader>Add folder</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <div>
-
-            <Button>Idk</Button>
+          <ModalHeader p={1}>
+            <div className='flex items-center justify-around gap-8'>
+              <p className='font-light text-[15px]'>| New folder</p>
+              <button className='font-normal text-[15px] px-4  hover:bg-slate-200 hover:border-none border-none py-2'>Add folder</button>
             </div>
-            <div>
-
-            <Button>Idk</Button>
-            </div><div>
-
-<Button>Idk</Button>
-</div>
+          </ModalHeader>
+          <ModalCloseButton className='font-light' />
+          <ModalBody>
+            <p className='text-left font-light'>Enter folder name</p>
+            <input 
+              type="text"
+              placeholder='Unset'
+              className='font-bold text-[50px] text-left pl-8 w-[100%] focus:outline-none'
+            />
           </ModalBody>
         </ModalContent>
       </Modal>
