21caf3636cb7b17cb7716006923b9c53db3e09e9
NurkenInc <nurkenkenes7@gmail.com>
2023-05-23 23:34:26 +0600

    implemented statusdropdown component

--- client/src/components/StatusDropdown/StatusDropdown.tsx
+++ client/src/components/StatusDropdown/StatusDropdown.tsx
@@ -1,19 +1,79 @@
-import React, { useState } from "react";
-import { animate, stagger, useAnimate } from "framer-motion";
+import React, { useState, useEffect } from "react";
+import { motion, animate, stagger, useAnimate } from "framer-motion";
 import { 
   AiFillCheckSquare, 
   AiFillCloseSquare, 
   AiFillClockCircle 
 } from 'react-icons/ai';
 
+const staggerDropdownItems = stagger(0.1, { startDelay: 0.15 });
+
+const useDropdownAnimation = (isOpen : boolean) => {
+  const [scope, animate] = useAnimate();
+  
+  useEffect(() => {
+    animate(".arrow", { rotate: isOpen ? 180 : 0 }, { duration: 0.2 });
+
+    animate(
+      "ul",
+      {
+        clipPath: isOpen
+          ? "inset(0% 0% 0% 0% round 10px)"
+          : "inset(10% 50% 90% 50% round 10px)"
+      },
+      {
+        type: "spring",
+        bounce: 0,
+        duration: 0.5
+      }
+    );
+
+    animate(
+      "li",
+      isOpen
+        ? { opacity: 1, scale: 1, filter: "blur(0px)" }
+        : { opacity: 0, scale: 0.3, filter: "blur(20px)" },
+      {
+        duration: 0.2,
+        delay: isOpen ? staggerDropdownItems : 0
+      }
+    );
+  }, [isOpen]);
+
+  return scope;
+}
+
 const StatusDropdown = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-
+  const scope = useDropdownAnimation(isOpen);
+  
   return (
-    <div>
-   
+    <div ref={scope} className='w-[300px] drop-shadow-[1px_1px_1px_#5116a9]'>
+      <motion.button
+        whileTap={{ scale: 0.95 }}
+        onClick={() => setIsOpen(!isOpen)}
+        className='w-[100%] px-[20px] py-[10px] font-bold bg-white border-none rounded-[10px] cursor-pointer text-left flex items-center mb-[10px]'
+      >
+        Status
+        <div className="arrow" style={{ transformOrigin: "50% 55%" }}>
+          <svg width="15" height="15" viewBox="0 0 20 20">
+            <path d="M0 7 L 20 7 L 10 16" />
+          </svg>
+        </div>
+      </motion.button>
+      <ul
+        style={{
+          pointerEvents: isOpen ? 'auto' : 'none',
+          clipPath: 'inset(10% 50% 90% 50% round 10px)'
+        }}
+        className='flex-col gap-[10px] bg-[white]'
+      >
+        <li className='block origin-[-20%_50%] text-[#9900ff]'>Done</li>
+        <li className='block origin-[-20%_50%] text-[#9900ff]'>In Progress</li>
+        <li className='block origin-[-20%_50%] text-[#9900ff]'>Not done</li>
+      </ul>
     </div>
-  );
+  )
 }
 
 export default StatusDropdown;
